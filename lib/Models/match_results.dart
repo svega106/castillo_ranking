b2c45ce879809e97e3fd4e565042642c0ddb1891
svega106 <svega106@gmail.com>
2020-02-04 16:06:30 -0600

    Match Results Widget modifications made. Still work needed to be done.
    Needs to add the database query to search results.
    
    Append user Display Names in order to see who.

--- lib/Models/match_results.dart
+++ lib/Models/match_results.dart
@@ -6,5 +6,63 @@ Widget matchResults(double screenWidth) {
     width: screenWidth,
     decoration: BoxDecoration(
         color: Colors.white, borderRadius: BorderRadius.circular(15.0)),
+    child: Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    width: 10.0,
+                    margin: EdgeInsets.only(
+                        right: 20.0, left: 20.0, top: 10.0, bottom: 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(15.0)),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: 10.0,
+                    margin: EdgeInsets.only(
+                        right: 20.0, left: 20.0, top: 10.0, bottom: 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(15.0)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    width: 10.0,
+                    margin: EdgeInsets.only(
+                        right: 20.0, left: 20.0, top: 10.0, bottom: 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(15.0)),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: 10.0,
+                    margin: EdgeInsets.only(
+                        right: 20.0, left: 20.0, top: 10.0, bottom: 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(15.0)),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
   );
 }
