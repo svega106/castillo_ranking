817100e9722a0a0a71b6c524e3e9ee9fbf71fcdc
Sebas Vega <svega106@gmail.com>
2020-02-10 23:44:50 -0600

    Small Change

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'Models/player.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<List<Player>>.value(
